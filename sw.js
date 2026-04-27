f6dd1de09a5c0a4676ecab5daa53dcf8229925d8
joaodasilva582-del <joaodasilva582@gmail.com>
2026-04-27 10:09:38 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
 self.addEventListener('fetch', () => {});
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+ 
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
